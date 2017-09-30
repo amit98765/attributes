a2473cdd380e73ae01fc8d1d7ba6a17ca861dffa
amit98765 <amitkumarmittalakm@gmail.com>
2017-09-30 10:50:22 +1000

    proper implementation as per example done, added few modifications as well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  colors: string[] = ["red", "blue", "green", "yellow"];
+  name: string;
+  color: string = "color";
+  isDisabled: boolean = true;
+
+  classes: string[] = ['bold', 'italic', 'highlight'];
+  selectedClass: string[] = [];
+
+  enabler(): void {
+    this.isDisabled = !this.isDisabled;
+    this.color = "black";
+  }
+
+  addClass(event: any) {
+    this.selectedClass = [];
+    var values = event.target.options;
+    var opt: any;
+
+    for (var i = 0; i < values.length; i++) {
+      opt = values[i];
+      if (opt.selected) {
+        this.selectedClass.push(opt.text);
+      }
+    }
+  }
 }
